3c1eb09210475da2354d4dc0d3f78e9b4ec04d2f
aykefamatgithub <aykefam@gmail.com>
2022-10-18 11:16:10 -0700

    q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_alphabet -prints lowercase alphabetes
+ */
+void print_alphabet(void)
+{
+	char alpha;
+	for(alpha='a';alpha <='z';alpha++)
+		_putchar(alpha);
+	_putchar('\n');
+}
